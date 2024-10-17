292bb719f1bc8b720d1890cb77131a49655a5016
Bennett Lee <bennettlee@Bennetts-MacBook-Pro.local>
2024-10-16 22:29:08 -0700

    add week 8

--- Week8/HomeworkAnimation/HomeworkAnimation/ContentView.swift
+++ Week8/HomeworkAnimation/HomeworkAnimation/ContentView.swift
@@ -8,17 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var yOffset = 0.0
+    @State var opacity = 1.0
     var body: some View {
+
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Rectangle()
+                .fill(Color.teal)
+                .frame(width: 300, height: 700)
+                .offset(y: yOffset)
+                .opacity(opacity)
+                .onTapGesture {
+                    animate()
+                }
         }
         .padding()
+        .toolbar {
+            Button("Animate"){
+                animate()
+            }
+        }
+    }
+
+    func animate(){
+        withAnimation(
+            .linear(duration: 0.5)
+        ) {
+            yOffset = -500
+            opacity = 0
+        }
+
+        withAnimation(
+            .linear(duration: 0.5)
+            .delay(0.5)
+        ) {
+            yOffset = 500
+        }
+        withAnimation(
+            .linear(duration: 0.5)
+            .delay(1.0)
+        ) {
+            yOffset = 0
+            opacity = 1.0
+        }
+
+
+
+//                withAnimation {
+//                } completion: {
+//                    withAnimation {
+//                    } completion: {
+//                        withAnimation {
+//                        } completion: {
+//                        }
+//                    }
+//                }
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
